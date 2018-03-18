3bebcd1e27d5a52f7be5ba3d801d8d90a4ca8b8f
jeremyyew <yewern@gmail.com>
2018-03-18 13:29:18 +0800

    Added individual post_detail page.

--- stuffshare/stuffshare/views/posts.py
+++ stuffshare/stuffshare/views/posts.py
@@ -10,7 +10,8 @@ def show_posts():
     for post in posts_rows:
         bids = db.execute(
             'select * from bids where post_id = ?', [post["id"]]).fetchall()
-        posts.append(dict(user_email=post["user_email"],
+        posts.append(dict(id=post["id"],
+                          user_email=post["user_email"],
                           title=post["title"],
                           price=post["price"],
                           desc=post["description"],
@@ -28,3 +29,17 @@ def add_post():
     db.commit()
     flash('New post was successfully created')
     return redirect(url_for('show_posts'))
+
+
+@app.route('/posts/<int:id>', methods=['GET'])
+def post_detail(id):
+    db = get_db()
+    post = db.execute(
+        'select * from posts where id = ?', [id]).fetchall()
+    bids = db.execute(
+        'select * from bids where post_id = ?', [id]).fetchall()
+    if not post:
+        flash("Sorry, that item does not exist")
+        return redirect(url_for('show_posts'))
+        # Note we access the first item in the list since we only expect one item
+    return render_template('post_detail.html', post=post[0], bids=bids)
