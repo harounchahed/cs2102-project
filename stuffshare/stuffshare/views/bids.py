48e4dc6c2284d39b5e9c44fdf8e75d0bf9cb2db7
jeremyyew <yewern@gmail.com>
2018-03-27 15:02:27 +0800

    Created user posts page. Implemented update bid, and delete posts. Remaining: update posts.

--- stuffshare/stuffshare/views/bids.py
+++ stuffshare/stuffshare/views/bids.py
@@ -5,11 +5,30 @@ from stuffshare import *
 def add_bid(post_id):
     if not session.get('logged_in'):
         abort(401)
-    db = get_db()
-    db.execute('insert into bids (user_email, post_id, offer) values (?, ?, ?)',
-               [session['user'], post_id, request.form['offer']])
-    db.commit()
-    flash('New bid successfully created.')
+    try:
+        db = get_db()
+        db.execute('insert into bids (user_email, post_id, offer) values (?, ?, ?)',
+                   [session['user'], post_id, request.form['offer']])
+        db.commit()
+        flash('New bid successfully created.')
+    except Exception as e:
+        flash(str(e))
+    return redirect(url_for('post_detail', id=post_id))
+
+@app.route('/update_bid/<string:user_email>/<int:post_id>', methods=['POST'])
+def update_bid(user_email, post_id):
+    if not session.get('logged_in'):
+        abort(401)
+    elif session['user'] != user_email:
+        flash("Sorry, you can only update your own bids.")
+    try:
+        db = get_db()
+        db.execute('update bids set offer = ? where user_email = ? and post_id = ?',
+                   [request.form['offer'], user_email, post_id])
+        db.commit()
+        flash('Bid successfully updated.')
+    except Exception as e:
+        flash(str(e))
     return redirect(url_for('post_detail', id=post_id))
 
 
